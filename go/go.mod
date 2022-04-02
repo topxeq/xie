8d4863219be3f9edf8c4bcf97ebe325582f85f2f
unknown <topxeq@163.com>
2022-04-02 09:28:51 +0800

    2022040201

---
+++ go/go.mod
@@ -0,0 +1,3 @@
+module github.com/topxeq/xie/go
+
+go 1.17
---
+++ go/xie.go
@@ -0,0 +1,15 @@
+package xie
+
+var versionG string = "0.0.1"
+
+func RunLine(lineA string) string {
+	return ""
+}
+
+func RunCode(codeA string) string {
+	return ""
+}
+
+func Version() string {
+	return versionG
+}
